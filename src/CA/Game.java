c41898089ff0c06191cff3614d942cdc909ae180
Sumer Vardhan <sv110@duke.edu>
2019-09-16 18:30:14 -0400

    Fixed error that came up on merging

--- src/CA/Game.java
+++ src/CA/Game.java
@@ -6,9 +6,11 @@ import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.util.Duration;
 
+import java.io.File;
+
 public class Game extends Application {
 
-    public static final String GAME_OF_LIFE_CONFIGURATION = "GameOfLife.txt";
+    public static final String GAME_OF_LIFE_CONFIGURATION = "Resources/GameOfLifeConfig.txt";
     public static final int FRAMES_PER_SECOND = 60;
     public static final int MILLISECOND_DELAY = 1000 / FRAMES_PER_SECOND;
     public static final double SECOND_DELAY = 1.0 / FRAMES_PER_SECOND;
@@ -18,8 +20,9 @@ public class Game extends Application {
     Visualization myVisualization;
 
     public Game() {
-        Grid grid = new Grid(GAME_OF_LIFE_CONFIGURATION);
-        myCells = grid.configureCell(); // make grid and populate grid of cells
+        File sim_file = new File(GAME_OF_LIFE_CONFIGURATION);
+        Grid grid = new Grid(sim_file);
+        myCells = grid.configureCells(); // make grid and populate grid of cells
         mySimulation = new GameOfLifeSimulation(myCells); //TODO: Move once we start having scene transitions
         myVisualization = new Visualization();
     }
