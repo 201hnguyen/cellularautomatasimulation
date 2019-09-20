6c3ddd807ecb6138c35c306223ce8020e747df35
SHREYA HURLI <sgh19@duke.edu>
2019-09-19 21:52:15 -0400

    wrote PercolationSimulation analyze method

---
+++ src/CA/PercolationSimulation.java
@@ -0,0 +1,41 @@
+package CA;
+
+import java.util.ArrayList;
+
+public class PercolationSimulation extends Simulation{
+    public static int OPEN = 0;
+    public static int FULL = 1;
+    public static int BLOCKED = 2;
+
+    public PercolationSimulation(Grid grid) {
+        super(grid);
+    }
+
+    @Override
+    public void analyzeCells(){
+        for(Cell[] cellRow : myGrid.getCells()){
+            for(Cell cell : cellRow){
+                Cell[] neighborsToFill = openNeighbors(cell, cell.getMyNeighbours());
+                if(cell.getState() == FULL && neighborsToFill.length != 0){
+                    for(Cell openNeighbor : neighborsToFill){
+                        openNeighbor.setMyNextState(FULL);
+                    }
+                }
+
+            }
+        }
+    }
+
+    public Cell[] openNeighbors(Cell curr, Cell[] neighbors){
+        ArrayList<Cell> openCells = new ArrayList<>();
+        for(Cell neighbor : neighbors){
+            if(neighbor.getState() == OPEN){
+                openCells.add(neighbor);
+            }
+        }
+        return openCells.toArray(new Cell[0]);
+    }
+
+
+
+}
