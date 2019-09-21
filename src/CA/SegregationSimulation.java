99b665c5a65677f69f417110449b7b1d96b4f267
Ha Nguyen <hn47@@duke.edu>
2019-09-21 16:34:30 -0400

    Resolved merge conflicts with SumerDev

--- src/CA/SegregationSimulation.java
+++ src/CA/SegregationSimulation.java
@@ -8,12 +8,9 @@ import java.util.Random;
 
 public class SegregationSimulation extends Simulation {
     private final double SEGREGATION_TRESHOLD = 0.5;
-<<<<<<< HEAD
     private ArrayList<Cell> availableCells;
-=======
     private final int AGENT1 = 1;
     private final int AGENT2 = 2;
->>>>>>> (Untested) working version of segregation; buttons for simulation in progress
 
     public SegregationSimulation(Grid grid) {
         super(grid);
@@ -29,36 +26,19 @@ public class SegregationSimulation extends Simulation {
                 double similarNeighbors = countSimilarNeighbors(cell, cell.getMyNeighbours());
                 if (cell.getState()!= 0 && (similarNeighbors / cell.getMyNeighbours().length < SEGREGATION_TRESHOLD)) {
                     System.out.println("My threshold: " + similarNeighbors / cell.getMyNeighbours().length);
-<<<<<<< HEAD
                     Cell random_cell = availableCells.get(random.nextInt(availableCells.size()));
                     random_cell.setMyIsAvailable(false);
                     random_cell.setMyNextState(cell.getState());
                     cell.setMyNextState(0);
                     availableCells.remove(random_cell);
                     availableCells.add(cell);
-=======
-                    Cell[] emptyCellsArray = myGrid.getEmptyCells();
-                    for(int i = 0; i < emptyCellsArray.length; i++) {
-                        if(emptyCellsArray[i].getMyIsAvailable()){
-                            emptyCellsArray[i].setMyNextState(cell.getState());
-                            emptyCellsArray[i].setMyIsAvailable(false);
-                            cell.setMyNextState(0);
-                            break;
-                        }
-                    }
->>>>>>> (Untested) working version of segregation; buttons for simulation in progress
                 }
             }
 
         }
     }
 
-<<<<<<< HEAD
 
-
-
-=======
->>>>>>> (Untested) working version of segregation; buttons for simulation in progress
     private double countSimilarNeighbors(Cell cell, Cell[] neighbors){
         int similarNeighborsCount = 0;
         int state = cell.getState();
